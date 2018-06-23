14ccb8a6d47d1b98d654e5d533d4328c1629b4f7
lovemyselfbest <commondirt@163.com>
2018-06-23 13:23:35 +0800

    Update App.java

--- src/main/java/com/git/test/App.java
+++ src/main/java/com/git/test/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!123999" );
+        System.out.println( "Hello World!123999111" );
 
         System.out.println( "Hello branch 2018!" );
 
